4eda008f91981057dfb50ba89123550c2f827648
cnicholas63 <cnicholas63@gmail.com>
2015-03-02 15:15:55 +0000

    Testing database repo

--- Risky-Business-Museum-App/app/src/main/java/riskybusiness/riskybusinessmuseumapp/root/Fragments/QRFragment.java
+++ Risky-Business-Museum-App/app/src/main/java/riskybusiness/riskybusinessmuseumapp/root/Fragments/QRFragment.java
@@ -20,6 +20,7 @@ import riskybusiness.riskybusinessmuseumapp.root.Activities.QRScannerActivity;
 
 /**
  * Created by Alex and Chris on 01/03/2015
+ * Database repo test
  */
 public class QRFragment extends Fragment {
     View OurView;
